7589c9f982673e340b003505328e27febf2004a8
vilka-9999 <ilia.volkov.05@gmail.com>
2025-07-14 17:50:15 -0400

    Updated Coin Calc

---
+++ code/constants.py
@@ -0,0 +1,56 @@
+TEAM_NAME_MAP = {
+    "atlanta hawks": "atlanta hawks",
+    "boston celtics": "boston celtics",
+    "brooklyn nets": "brooklyn nets",
+    "new jersey nets": "brooklyn nets", 
+    "charlotte hornets": "charlotte hornets",
+    "charlotte bobcats": "charlotte hornets", 
+    "chicago bulls": "chicago bulls",
+    "cleveland cavaliers": "cleveland cavaliers",
+    "dallas mavericks": "dallas mavericks",
+    "denver nuggets": "denver nuggets",
+    "detroit pistons": "detroit pistons",
+    "golden state warriors": "golden state warriors",
+    "houston rockets": "houston rockets",
+    "indiana pacers": "indiana pacers",
+    "los angeles clippers": "los angeles clippers",
+    "san diego clippers": "los angeles clippers", 
+    "los angeles lakers": "los angeles lakers",
+    "memphis grizzlies": "memphis grizzlies",
+    "vancouver grizzlies": "memphis grizzlies",
+    "miami heat": "miami heat",
+    "milwaukee bucks": "milwaukee bucks",
+    "minnesota timberwolves": "minnesota timberwolves",
+    "new orleans pelicans": "new orleans pelicans",
+    "new orleans hornets": "new orleans pelicans", 
+    "new orleansoklahoma city hornets": "new orleans pelicans", 
+    "new york knicks": "new york knicks",
+    "oklahoma city thunder": "oklahoma city thunder",
+    "seattle supersonics": "oklahoma city thunder",
+    "orlando magic": "orlando magic",
+    "philadelphia 76ers": "philadelphia 76ers",
+    "philadelphia ers": "philadelphia 76ers", 
+    "philadelphia sixers": "philadelphia 76ers", 
+    "phoenix suns": "phoenix suns",
+    "portland trail blazers": "portland trail blazers",
+    "sacramento kings": "sacramento kings",
+    "kansas city kings": "sacramento kings", 
+    "san antonio spurs": "san antonio spurs",
+    "toronto raptors": "toronto raptors",
+    "utah jazz": "utah jazz",
+    "washington wizards": "washington wizards",
+    "washington bullets": "washington wizards",
+}
+
+
+FIRST_PICK_COINS_COEF = 0
+SECOND_PICK_COINS_COEF = 0.25
+THIRD_PICK_COINS_COEF = 0.5
+FOURTH_PICK_COINS_COEF = 0.75
+
+NO_PLAYOFFS_BONUS = 1
+ROUND_1_LOSS_COEF = 1
+ROUND_2_LOSS_COEF = 0.75
+ROUND_3_LOSS_COEF = 0.5
+FINAL_LOSS_COEF = 0.25
+CHAMPION_COEF = 0
